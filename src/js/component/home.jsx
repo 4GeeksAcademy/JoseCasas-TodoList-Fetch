2826acff386149892c8a635e657d5ae16fc1a7a3
JC-kip <jjcasasm16@gmail.com>
2024-10-11 23:58:02 +0000

    Finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { List } from "./List";
 import { Input } from "./input";
 
 const Home = () => {
 	const [todos, setTodos] = useState([])
 	const deleteTodo = (index) => {
-        const newTodos = todos.filter((_, i) => i !== index);
-        setTodos(newTodos)
-    }
+		const newTodos = todos.filter((_, i) => i !== index);
+		setTodos(newTodos)
+	}
+	useEffect(() => {
+		async function createUser() {
+			let response = await fetch("https://playground.4geeks.com/todo/users/JoseCasas")
+			let data = await response.json()
+			if (data.detail == "Agenda \"JoseCasas\" doesn't exist.") {
+				let response = await fetch("https://playground.4geeks.com/todo/users/JoseCasas", {
+					method: "POST"
+				})
+				let data = response.json()
+			}
+			else {
+				setTodos(data.todos)
+			}
+
+		}
+	}, [])
 	return (
 		<div className="Page text-center">
 			<h1 className="title">To do List :</h1>
 			<Input todos={todos} setTodos={setTodos} />
-			<List todos={todos} setTodos={setTodos}	deleteTodo={deleteTodo} />
+			<List todos={todos} setTodos={setTodos} deleteTodo={deleteTodo} />
 		</div>
 	);
 };
