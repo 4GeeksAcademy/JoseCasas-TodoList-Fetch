b9b7ee0c05d9edb125f90988fb9563a5bb519fcc
JC-kip <jjcasasm16@gmail.com>
2024-10-11 03:33:38 +0000

    giving errors

--- src/js/component/List.jsx
+++ src/js/component/List.jsx
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export const List = ({ todos, setTodos, deleteTodo }) => {
+export const List = ({ todos, setTodos, deleteTodo}) => {
+
+    useEffect(() => {
+        fetch('https://playground.4geeks.com/todo/users/JoseCasas')
+        .then(response => response.json())
+        .then(data => {
+            setTodos(data.todos)
+        })
+        .catch(error => console.log("Error: ", error))
+    }, [])
+
+    const deleteTask = async (selectedTodoId) => {
+        let updatedTodos = todos.filter(todo => todo.id !== selectedTodoId);
+        setTodos(updatedTodos);
+
+        const response = await fetch(`https://playground.4geeks.com/todo/todos/${selectedTodoId}` , {
+            method: 'DELETE' 
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.log('Error: ', response.status, response.statusText);
+            return {
+                error: {
+                    status: response.status,
+                    statusText: response.statusText
+                }
+            }
+        }
+    }
+
     return (
         <ul className="list-group">
-            {todos.map((item, index) => {
-                return <li className="list-group-item">{item.label} <button className="trash" onClick={() => deleteTodo(index)}>❌</button></li>
+            {todos.map((todo) => {
+                return <li key={todo.id} className="list-group-item">{todo.label} <button className="trash" onClick={() => deleteTask(todo.id)}>❌</button></li>
             })}
             <li className="list-group-item">{todos.length == 0 ? "No Task, add a Task" : todos.length == 1 ? "1 Item left" : todos.length + " Items left"}</li>
         </ul>
--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
 
 export const Input = ({ todos, setTodos }) => {
+
     const [inputText, setInputText] = useState("")
-    const addTodo = (e) => {
-        if (e.key === "Enter" && inputText.trim() !== "") {
-            let newTodos = todos.concat({ label: inputText, done: false })
-            setTodos(newTodos)
-            setInputText("")
+
+    const addTodo = async (todoObject) => {
+        const options = {
+            method: "POST",
+            body: JSON.stringify(todoObject),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const response = await fetch('https://playground.4geeks.com/todo/todos/JoseCasas', options)
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        } else {
+            console.log('Error: ', response.status, response.statusText);
+            return {
+                error: {
+                    status: response.status,
+                    statusText: response.statusText
+                }
+            }
         }
     }
-   
+
+    const addTask = (e) => {
+        if (e.key === "Enter" && inputText.trim() !== "") {
+            const newTodoObject = { label: inputText, is_done: false };
+            const newTodos = [...todos, newTodoObject];
+            setTodos(newTodos);
+            addTodo(newTodoObject);
+            setInputText("");
+        }
+    }
+
     return (
 
-        <input value={inputText} placeholder="Add a task..." onChange={(e) => setInputText(e.target.value)} onKeyDown={(e) => addTodo(e)}/>
+        <input value={inputText} placeholder="Add a task..." onChange={(e) => setInputText(e.target.value)} onKeyDown={(e) => addTask(e)} />
     )
 }
